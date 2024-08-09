b035ff55ef0919d510b8a1b189367f5870c8e913
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-10 02:53:56 +0800

    unit test

---
+++ UnitTestProject1/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a9c4754d-42db-44e1-b4e9-bda359aa2f25")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Configuration;
+using AWAD_Assignment;
+using AWAD_Assignment.routes;
+using Salt_Password_Sample;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // Mock or initialize the necessary resources
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var email = "admin@email.com"; 
+
+            // Call the method and store the result
+            var result = Function.CheckIfUserExists(email);
+
+            // Add your assertions here based on the expected result
+           Assert.IsTrue( result == 1); // Or whatever your expected result is
+        }
+
+        
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var a = Function.IsUserAdmin("admin@email.com");
+            Assert.AreEqual( "True", a );
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //C
+            var email = "TestingAccount@email.com";
+            Function.RegisterUser(email,DBNull.Value,DBNull.Value,DBNull.Value,"tom","lee","85069025",Hash.ComputeHash("testpassword", "SHA512", null));
+
+            var result = Function.CheckIfUserExists(email);
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //U
+            var ema = "TestingAccount@email.com";
+            Function.SetUserVerificationTrue(ema);
+            var acc =Account.GetAccount(ema);
+            Assert.IsTrue(acc.emailConfirmed == true);
+        }
+        [TestMethod]
+        public void TestMethod5() {
+            // Del
+            var email = "TestingAccount@email.com";
+            Function.RemoveUserVoid(email);
+            var result = Function.CheckIfUserExists(email);
+            Assert.IsTrue(result == 0);
+        }
+    }
+
+
+}
